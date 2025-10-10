a6767f9d123ea46484d55f4bcb1a08bb33da8b67
aiyojun <1608450902@qq.com>
2025-10-10 17:19:29 +0800

    dev: remove UnserializeIPEndPoint method

--- FiberDistro/Core/Helper.cs
+++ FiberDistro/Core/Helper.cs
@@ -1,17 +1,9 @@
 ﻿using System.Net;
-using System.Net.Sockets;
 
 namespace FiberDistro.Core;
 
 public static class Helper
 {
-    public static IPEndPoint UnserializeIPEndPoint(ReadOnlySpan<byte> buf)
-    {
-        var socketAddress = new SocketAddress(buf.Length == 16 ? AddressFamily.InterNetwork : AddressFamily.InterNetworkV6, buf.Length);
-        for (var i = 0; i < buf.Length; i++) { socketAddress[i] = buf[i]; }
-        return (IPEndPoint) new IPEndPoint(0, 0).Create(socketAddress);
-    }
-    
     public static string GetNetworkAddress(string network)
     {
         if (network[^1] != '.') 
